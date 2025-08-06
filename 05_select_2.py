c2d7a7c9e707865ac21f41bc3ea02da488762e4d
choseongchan3712 <choseongchan3712@naver.com>
2025-08-07 03:12:54 +0900

    select_2

---
+++ 05_select_2.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://workey.codeit.kr/ratings/index")
+rating_page = response.text
+
+soup = BeautifulSoup(rating_page, 'html.parser')
+
+print(soup.select('td')[:4]) # ? [:4] -> 리스트 슬라이싱
+# [<td class="rank">1</td>, <td class="channel">KBS2</td>, <td class="program">주말연속극(수상한삼형제)</td>, <td class="percent">33.4</td>]
+
+td_tags = soup.select('td')[:4]
+for tag in td_tags:
+    print(tag.get_text())
+# 1
+# KBS2
+# 주말연속극(수상한삼형제)
+# 33.4
